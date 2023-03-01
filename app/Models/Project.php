81e4318044cde628bde36921de6a560467f345bd
cerry24 <samuelecerretti@gmail.com>
2023-03-01 15:26:10 +0100

    first commit

---
+++ app/Models/Project.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = array('title', 'slug', 'thumbnail', 'description', 'creation_date', 'type_id');
+
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
+
+    /**
+    * Get the route key for the model.
+    *
+    * @return string
+    */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function isThumbnailAUrl() {
+        return filter_var($this->thumbnail, FILTER_VALIDATE_URL);
+    }
+}
---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use illuminate\support\Str;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i <200 ; $i++) { 
+            $newProject = new Project();
+            $newProject->type_id = Type::inRandomOrder()->first()->id;
+            $newProject->title = $faker->unique()->realTextBetween(5, 40);
+            $newProject->slug = Str::slug($newProject->title);
+            $newProject->thumbnail = $faker->imageUrl(550, 300);
+            $newProject->description = $faker->realTextBetween(50, 300);
+            $newProject->creation_date = $faker->dateTimeThisDecade();
+            $newProject->save();
+        }
+    }
+}
